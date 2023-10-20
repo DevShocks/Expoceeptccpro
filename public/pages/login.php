9d4810a2d5b199b72cc919f0e74ef3a7aab3265b
DevShocks <matheusrodrigues58277@gmail.com>
2023-10-20 18:36:46 -0300

    parte minha

--- public/pages/login.php
+++ public/pages/login.php
@@ -3,17 +3,19 @@
     <div class="login-header">
       <div>Login</div>
     </div>
-    <input type="text" class="login-input" placeholder="Usuário" id="username">
-    <input type="password" class="login-input" placeholder="Senha (Min-8, Max-15)" id="password" maxlength="15" minlength="8">
-    <button class="login-button" id="login-button">Login</button>
+    <form action="" id="form">
+    <input type="text" class="login-input" placeholder="Usuário" id="name">
+    <input type="password" class="login-input" placeholder="Senha (Min-8, Max-15)" id="senha" maxlength="15" minlength="8">
+    <button class="login-button" id="login-button" onclick="logar()">Login</button>
     <br><br>
     <p class="registrolink">
       Sem conta?
       <a href="?page=cadastro">Cadastrar</a>
     </p>
     <p class="registrolink">
-      Entrar como 
+      Entrar como
       <a href="?page=professorescadastro">professor</a>
-    </p>
+    </p></form>
   </div>
+  <script src="assets/js/logicalogin.js"></script>
 </center>
